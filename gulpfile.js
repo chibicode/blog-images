3c27151b177d602dafb7d7bbdc0b39aae0563b7d
Shu Uesugi <shu@chibicode.com>
2015-01-31 12:17:40 -0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// gulpfile.js based on Google Web Starter Kit:
+// https://github.com/google/web-starter-kit/blob/master/gulpfile.js
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+
+gulp.task('images', function() {
+  return gulp.src(['src/**/*'])
+    .pipe($.size({ title: "before" }))
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist'))
+    .pipe($.debug())
+    .pipe($.size({ title: "after" }));
+});
+
+gulp.task('clearCache', function (done) {
+  return $.cache.clearAll(done);
+});
+
+gulp.task('clean', del.bind(null,
+  ['dist/**/*'],
+  {dot: true, force: true}));
+
+gulp.task('watch', function () {
+  gulp.watch(['src/**/*'],
+    ['images']);
+});
+
+gulp.task('default', ['clean', 'clearCache', 'images', 'watch']);
